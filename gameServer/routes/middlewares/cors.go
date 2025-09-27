8a383d9d79d35b0c0a2bbfe2f704c40c7622ae15
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2025-09-27 20:34:32 -0300

    refatora middleware CORS para simplificar a configuração de cabeçalhos e remover chamada desnecessária ao próximo handler

--- gameServer/routes/middlewares/cors.go
+++ gameServer/routes/middlewares/cors.go
@@ -9,15 +9,16 @@ import (
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if os.Getenv("CORS") == "false" {
-			next.ServeHTTP(w, r)
-			return
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "*")
+			w.Header().Set("Access-Control-Allow-Headers", "*")
+		} else {
+			// Configura cabeçalhos CORS
+			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
+			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("GAME_SERVER_URL"))
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		}
-
-		// Configura cabeçalhos CORS
-		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
-		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("GAME_SERVER_URL"))
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Responde a preflight diretamente
 		if r.Method == http.MethodOptions {
