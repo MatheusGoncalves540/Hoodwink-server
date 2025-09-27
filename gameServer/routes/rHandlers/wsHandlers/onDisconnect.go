6b8d0c8a9ff57a5d1c2987d14e175028264b8d0d
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2025-09-27 07:24:26 -0300

    atualiza lógica de desconexão para remover jogador da sala apenas se o jogo não estiver iniciado

--- gameServer/routes/rHandlers/wsHandlers/onDisconnect.go
+++ gameServer/routes/rHandlers/wsHandlers/onDisconnect.go
@@ -2,9 +2,10 @@ package wsHandlers
 
 import (
 	"context"
-	"log"
+	"time"
 
 	"github.com/MatheusGoncalves540/Hoodwink-gameServer/game/room/redisHandlers"
+	"github.com/MatheusGoncalves540/Hoodwink-gameServer/utils"
 	"github.com/gorilla/websocket"
 	"github.com/redis/go-redis/v9"
 )
@@ -14,7 +15,20 @@ func OnDisconnect(conn *websocket.Conn, ctx context.Context, rdb *redis.Client, 
 	redisHandlers.AcquirePlayerLock(ctx, rdb, playerId, 0)
 	defer redisHandlers.ReleasePlayerLock(ctx, rdb, playerId)
 
-	redisHandlers.UnregisterPlayerFromRoom(ctx, rdb, playerId)
-	log.Println("Cliente desconectado do WebSocket")
+	// Remove o registro do player na sala apenas se o jogo não estiver iniciado
+	startTimeAny, err := redisHandlers.LoadRoomField(ctx, rdb, roomId, "StartTime")
+	if err != nil {
+		utils.LogDebug("Erro ao verificar horário de início da sala:" + err.Error())
+		return
+	}
+	startTime, ok := startTimeAny.(time.Time)
+	if !ok {
+		utils.LogDebug("Erro: StartTime não é do tipo time.Time")
+		return
+	}
+	if startTime.IsZero() {
+		redisHandlers.UnregisterPlayerFromRoom(ctx, rdb, playerId)
+	}
+	utils.LogDebug("Cliente desconectado do WebSocket")
 	// Aqui você pode adicionar lógica extra, como limpar recursos
 }
