2e1b9bc2548033725db38b886993b9d47ae70bfd
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2026-01-15 17:25:00 -0300

    adiciona suporte para modo de depuração inseguro através da variável de ambiente UNSAFE_DEBUG

--- gameServer/utils/envUtils.go
+++ gameServer/utils/envUtils.go
@@ -10,6 +10,10 @@ func IsDebugMode() bool {
 	return os.Getenv("DEBUG") == "true"
 }
 
+func IsUnsafeDebugMode() bool {
+	return os.Getenv("UNSAFE_DEBUG") == "true"
+}
+
 // MustEnvInt lê uma variável de ambiente e converte para int, ou retorna valor padrão se não definida ou inválida.
 func MustEnvInt(key string, def int) int {
 	if v := os.Getenv(key); v != "" {
