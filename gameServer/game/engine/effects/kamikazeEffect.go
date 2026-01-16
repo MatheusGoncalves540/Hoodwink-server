09f411306b760953c7346a39dca4485871bb3551
Matheus Gonçalves <matheussouzagoncalves540@gmail.com>
2026-01-15 21:57:57 -0300

    remove: elimina validação do efeito Kamikaze

--- gameServer/game/engine/effects/kamikazeEffect.go
+++ gameServer/game/engine/effects/kamikazeEffect.go
@@ -3,7 +3,6 @@ package effects
 import (
 	"context"
 
-	"github.com/MatheusGoncalves540/Hoodwink-gameServer/game/engine/effects/effectsValidations"
 	"github.com/MatheusGoncalves540/Hoodwink-gameServer/game/rules"
 	"github.com/MatheusGoncalves540/Hoodwink-gameServer/game/structs"
 	"github.com/MatheusGoncalves540/Hoodwink-gameServer/game/structs/rooms"
@@ -12,19 +11,6 @@ import (
 )
 
 func KamikazeEffect(ctx context.Context, rdb *redis.Client, RegistryRules *rules.Registry, roomData *rooms.Room, effect structs.Effect) {
-	player, err := roomData.GetPlayer(effect.SourcePlayer)
-	if err != nil {
-		utils.LogError(err)
-		return
-	}
-
-	// valida a jogada do jogador
-	valid, err := effectsValidations.ValidateKamikaze(player)
-	if err != nil || !valid {
-		utils.LogInvldPlyrReq(err, player.Id)
-		return
-	}
-
 	// resolve o efeito de matar carta
 	err = KillCard(ctx, rdb, RegistryRules, roomData, effect)
 	if err != nil {
